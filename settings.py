aaed65b5a09ade00aa5a533799ce7ad27be4fb99
aldrynbot <aldrynbot@1744d0880826>
2018-05-18 18:14:03 +0000

    install aldryn-video 1.0.0

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ INSTALLED_ADDONS = [
     'aldryn-sso',
     'aldryn-django-cms',
     'aldryn-devsync',
+    'aldryn-video',
     'djangocms-bootstrap4',
     'djangocms-file',
     'djangocms-googlemap',
